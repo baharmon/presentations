7645a1b696f99438c0cf0a9dfb1ebff96002a1fa
Brendan <brendan.harmon@gmail.com>
2019-10-12 10:18:03 -0500

    cleaned up repo

--- README.md
+++ README.md
@@ -2,10 +2,9 @@ A collection of presentations and talks
 ===========================================
  * https://baharmon.github.io/presentations/robots-in-nature.html
  * https://baharmon.github.io/presentations/kinetic-art.html
+ * https://baharmon.github.io/presentations/tangible-landscape-2017.html
+ * https://baharmon.github.io/presentations/tangible-landscape-2019.html
  * https://baharmon.github.io/presentations/generative-landscape-architecture.html
- * https://baharmon.github.io/presentations/lecture-2017.html
- * https://baharmon.github.io/presentations/cartography-2017.html
- * https://baharmon.github.io/presentations/doctoral-defense.html
 
 Building and publishing pages for this repository
 -------------------------------------------------
@@ -18,7 +17,7 @@ Navigate to the first clone and build pages::
 
     ./build.sh
 
-To add new presentation, create new HTML file and edit `./build-slides.py`.
+To add a new presentation, create new HTML file and edit `./build-slides.py`.
 
 To publish your changes, do:
 
@@ -30,7 +29,7 @@ To publish your changes, do:
 About the slides
 ----------------
 
-Presentation is using on Reveal.js HTML Presentation Framework.
+These presentations use the Reveal.js HTML Presentation Framework.
 
  * http://lab.hakim.se/reveal-js/#/
  * https://github.com/hakimel/reveal.js/
@@ -38,7 +37,7 @@ Presentation is using on Reveal.js HTML Presentation Framework.
 License
 -------
 
-The presentation text and images are under the CC BY-SA 4.0 license.
+The presentations' text and images are under the CC BY-SA 4.0 license.
 
-Note that many things are linked material, logos, data, fonts, styles
+Note that many things are linked material, logos, data, fonts, or styles
 from different sources which may have different licenses.
--- build.sh
+++ build.sh
@@ -3,44 +3,38 @@
 ./build-slides.py --outdir=build \
     --outfile=doctoral-defense.html \
     --title="Tangible Landscape" \
-    --meta-description="Slides for doctoral defense presentation" \
+    --meta-description="Doctoral Defense Presentation" \
     doctoral-defense.html
 
 ./build-slides.py --outdir=build \
-    --outfile=lecture-2017.html \
+    --outfile=tangible-landscape-2017.html \
     --title="Tangible Landscape" \
-    --meta-description="Tangible Landscape lecture slides, Spring 2017" \
-    lecture-2017.html
-
-./build-slides.py --outdir=build \
-    --outfile=cartography-2017.html \
-    --title="Cartography and GIS" \
-    --meta-description="Cartography and GIS lecture, Spring 2017" \
-    cartography-2017.html
+    --meta-description="Tangible Landscape Lecture, Spring 2017" \
+    tangible-landscape-2017.html
 
 ./build-slides.py --outdir=build \
     --outfile=generative-landscape-architecture.html \
-    --title="Generative landscape architecture" \
-    --meta-description="Lecture on generative landscape architecture" \
+    --title="Generative Landscape Architecture" \
+    --meta-description="Lecture on Generative Landscape Architecture" \
     generative-landscape-architecture.html
-
-./build-slides.py --outdir=build \
-    --outfile=computational-ecology.html \
-    --title="Computational Ecology" \
-    --meta-description="Computational Ecology" \
-  computational-ecology.html
 
 ./build-slides.py --outdir=build \
     --outfile=kinetic-art.html \
     --title="Kinetic Art" \
     --meta-description="Lecture on Kinetic Art" \
-  kinetic-art.html
+    kinetic-art.html
 
-  ./build-slides.py --outdir=build \
-      --outfile=robots-in-nature.html \
-      --title="Robots in Nature" \
-      --meta-description="Robots in Nature" \
+./build-slides.py --outdir=build \
+    --outfile=robots-in-nature.html \
+    --title="Robots in Nature" \
+    --meta-description="Robots in Nature Proposal" \
     robots-in-nature.html
+
+./build-slides.py --outdir=build \
+    --outfile=tangible-landscape-2019.html \
+    --title="Tangible Landscape" \
+    --meta-description="Tangible Landscape Lecture, Fall 2019" \
+    tangible-landscape-2019.html
 
 ## ADD ANOTHER PRESENTATION
 # ./build-slides.py --outdir=build \
